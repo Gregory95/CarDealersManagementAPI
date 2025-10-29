b82c8cbff513222f2083872598e67a5631fd373c
gkokozides <kokozides.g@outlook.com>
2025-10-29 23:27:30 +0200

    Fix migrations and database connection

---
+++ CarsDealersManagement.Infrastructure/Data/AppDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace CarsDealersManagement.Infrastructure.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+            optionsBuilder.UseSqlServer("Server=localhost\\SQLSERVER_LOCAL;Database=CarsDealersManagementDb;User Id=sa;Password=\"5w_vhAJ%3FNoM/n1;NxH\";TrustServerCertificate=True;");
+
+            optionsBuilder.UseOpenIddict();
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
