fbf0dd5a0dbfa32b940a7ab5797205354e97869f
gkokozides <kokozides.g@outlook.com>
2025-10-31 20:33:59 +0200

    Config updates

--- CarsDealersManagement.Microservice/Program.cs
+++ CarsDealersManagement.Microservice/Program.cs
@@ -2,7 +2,6 @@ using CarsDealersManagement.Infrastructure;
 using CarsDealersManagement.Infrastructure.Data;
 using CarsDealersManagement.Microservice.Middlewares;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 ConfigurationManager configuration = builder.Configuration;
